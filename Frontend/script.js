40b5dd5f384e8a43bb13a0c3aa092482dcc7fcd4
Aneree Patel <imanereepatel@gmail.com>
2026-04-20 01:00:24 +0530

    Update docker compose and frontend changes

--- Frontend/script.js
+++ Frontend/script.js
@@ -7,7 +7,7 @@ Chart.defaults.font.family = "'Inter', sans-serif";
 
 async function fetchData() {
     try {
-        const res = await fetch("http://3.7.48.90:8000/api/spills");
+        const res = await fetch("http://127.0.0.1:8000/api/spills");
         const data = await res.json();
 
         if (!data) {
